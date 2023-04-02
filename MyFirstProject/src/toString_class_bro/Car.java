b637cf7233c2b322ebfdea21398cd256ac143736
Caio <caiocbahia@hotmail.com>
2023-04-02 20:21:37 -0300

    overriding method toString()

---
+++ MyFirstProject/src/toString_class_bro/Car.java
@@ -0,0 +1,16 @@
+package toString_class_bro;
+
+public class Car {
+
+	String make = "Toyota";
+	String model = "Hilux";
+	String color = "White";
+	int year = 2020;
+	
+	public String toString() {
+		
+		return make+"\n"+model+"\n"+color+"\n"+year;
+		
+	}
+
+}
---
+++ MyFirstProject/src/toString_class_bro/Main.java
@@ -0,0 +1,16 @@
+package toString_class_bro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Car car = new Car();
+		
+//in this exercise, we're overriding the toString() method
+//note: with println, toString() is automatically called
+		
+		System.out.println(car.toString());
+
+	}
+
+}
